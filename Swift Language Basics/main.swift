b440bb6bed1c64ad663e4139936df5289b92d95d
Holt Skinner <HoltWasHere@gmail.com>
2017-09-02 12:49:32 -0500

    Initial Commit

---
+++ Swift Language Basics/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Swift Language Basics
+//
+//  Created by Holt Skinner on 9/2/17.
+//  Copyright © 2017 Holt Skinner. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
